c79cae768139eab11ebf6778b80e3395afbeedc4
derek-perry <dp@derek-perry.com>
2024-07-08 21:26:42 -0400

    Update API
    Updated API to grab from the new Strapi instance on Railway that actually seems to work this time.

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -6,12 +6,14 @@ import SiteHead from '../components/SiteHead';
 import PageFooter from '../components/PageFooter';
 
 interface eventProps {
-  Name: string,
-  StartTime: string,
-  EndTime: string,
-  Price: string,
-  Description: string,
-  Image: eventImageProps
+  id: number;
+  attributes: {
+    Name: string,
+    Date: string,
+    Price: string,
+    Description: string,
+    Image: eventImageProps
+  }
 };
 
 interface eventImageProps {
@@ -59,7 +61,7 @@ const calendarPage: NextPage = () => {
           setIsLoading(true);
           const fetchedData = [];
           const { data } = await api.get(
-            'ari-events?pagination[page]=1&pagination[pageSize]=10&sort[0]=name:asc&populate[Image][fields][0]=height&populate[Image][fields][1]=width&populate[Image][fields][2]=url&fields[0]=Name&fields[1]=Description&fields[2]=Price&fields[3]=StartTime&fields[4]=EndTime'
+            'events?pagination[page]=1&pagination[pageSize]=10&sort[0]=id:desc&populate[Image][fields][0]=height&populate[Image][fields][1]=width&populate[Image][fields][2]=url&populate[Image][fields][3]=alternativeText&fields[0]=Name&fields[1]=Description&fields[2]=Price&fields[3]=Date'
           );
           fetchedData.push(...data?.data);
           if (
@@ -70,7 +72,7 @@ const calendarPage: NextPage = () => {
             const { page, pageCount } = data?.meta?.pagination;
             for (let i = page + 1; i <= pageCount; i++) {
               let response = await api.get(
-                `ari-events?pagination[page]=${i}&pagination[pageSize]=10&sort[0]=name:asc&populate[Image][fields][0]=height&populate[Image][fields][1]=width&populate[Image][fields][2]=url&fields[0]=Name&fields[1]=Description&fields[2]=Price&fields[3]=StartTime&fields[4]=EndTime`
+                `events?pagination[page]=${i}&pagination[pageSize]=10&sort[0]=id:desc&populate[Image][fields][0]=height&populate[Image][fields][1]=width&populate[Image][fields][2]=url&populate[Image][fields][3]=alternativeText&fields[0]=Name&fields[1]=Description&fields[2]=Price&fields[3]=Date`
               );
               fetchedData.push(...response.data.data);
             };
@@ -90,11 +92,11 @@ const calendarPage: NextPage = () => {
   );
 
   function stringWithLineBreaks(inputString: string) {
-    var outputString = inputString.toString().replace(/(?:\r\n|\r|\n)/g, '<br />');
+    var outputString = inputString?.toString().replace(/(?:\r\n|\r|\n)/g, '<br />');
     return outputString;
   };
   function stringWithUrlSupport(inputString: string) {
-    var outputString = inputString.trim().toString().toLowerCase().replace(/\s+/g, '-').replace(/ - /g, "-").replace(/---/g, "-").replace(/\&/g, "and").replace(/;/g, "%3B").replace(/:/g, "%3A").replace(/"/g, "%22").replace(/'/g, "%27").replace(/,/g, "%2C").replace(/\?/g, "%3F").replace(/!/g, "%21").replace(/@/g, "%40").replace(/#/g, "%23").replace(/\$/g, "%24").replace(/&/g, "%26").replace(/\*/g, "%2A").replace(/=/g, "%3D").replace(/\+/g, "%2B").replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/\[/g, "%5B").replace(/\]/g, "%5D").replace(/\\/g, "%5C").replace(/\//g, "%2F");
+    var outputString = inputString?.trim().toString().toLowerCase().replace(/\s+/g, '-').replace(/ - /g, "-").replace(/---/g, "-").replace(/\&/g, "and").replace(/;/g, "%3B").replace(/:/g, "%3A").replace(/"/g, "%22").replace(/'/g, "%27").replace(/,/g, "%2C").replace(/\?/g, "%3F").replace(/!/g, "%21").replace(/@/g, "%40").replace(/#/g, "%23").replace(/\$/g, "%24").replace(/&/g, "%26").replace(/\*/g, "%2A").replace(/=/g, "%3D").replace(/\+/g, "%2B").replace(/\(/g, "%28").replace(/\)/g, "%29").replace(/\[/g, "%5B").replace(/\]/g, "%5D").replace(/\\/g, "%5C").replace(/\//g, "%2F");
     return outputString;
   };
   return (
@@ -111,39 +113,37 @@ const calendarPage: NextPage = () => {
         <section id="events">
           <h2 className="mb-4">Upcoming Events</h2>
           <div className="mb-28 max-w-[1080px] w-full overflow-hidden flex flex-row flex-wrap gap-16 items-top justify-center text-center text-xl">
-            {isLoading ? (
-              <p>Loading Events...</p>
-            ) : (events && events.length ?
-              (events
-                .slice(1)
-                .map(({ Name, StartTime, EndTime, Price, Description, Image }) => (
+
+          {isLoading ? (
+            <p>Loading Events...</p>
+          ) : (
+            (events ? (
+              <div className='flex flex-row flex-wrap gap-3 justify-center align-middle items-center text-center'>
+                {events.map((event) => (
                   <article
                     className="bg-[#1c1c1a] rounded-t-md overflow-hidden w-full max-w-full"
-                    key={Name}
-                    id={stringWithUrlSupport(Name)}
+                    key={event.attributes.Name}
+                    id={stringWithUrlSupport(event.attributes.Name)}
                   >
-                    {Image.data ? (
+                    {event.attributes.Image && event.attributes.Image.data ? (
                       <div className='min-w-auto max-w-[240px] sm:min-w-[300px] sm:max-w-[300px] p-3'>
                         <img
                           className='shadow-musicCard'
-                          alt={Image.data?.attributes.alternativeText}
-                          src={Image.data?.attributes.url ? Image.data?.attributes.url : ''}
-                          height={Image.data?.attributes.height ? Image.data?.attributes.height : 0}
-                          width={Image.data?.attributes.width ? Image.data?.attributes.width : 0}
+                          alt={event.attributes.Image.data?.attributes.alternativeText}
+                          src={event.attributes.Image.data?.attributes.url ? event.attributes.Image.data?.attributes.url : ''}
+                          height={event.attributes.Image.data?.attributes.height ? event.attributes.Image.data?.attributes.height : 0}
+                          width={event.attributes.Image.data?.attributes.width ? event.attributes.Image.data?.attributes.width : 0}
                         />
                       </div>
                     ) : ''}
-                    <h3 className="bg-ariWhite text-ariBlack min-h-[88px] flex items-center justify-center font-bold text-3xl px-5 max-sm:hyphens-auto">{Name}</h3>
-                    <p className="pt-2 bg-ariWhite text-ariBlack max-sm:hyphens-auto text-2xl px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(StartTime) }} />
-                    <p className="pt-2 bg-ariWhite text-ariBlack max-sm:hyphens-auto text-2xl px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(EndTime) }} />
-                    <p className="bg-ariWhite text-ariBlack py-4 max-sm:hyphens-auto text-2xl px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(Price) }} />
-                    <p className="my-4 max-sm:hyphens-auto text-left px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(Description) }} />
+                    <h3 className="bg-ariWhite text-ariBlack min-h-[88px] flex items-center justify-center font-bold text-3xl px-5 max-sm:hyphens-auto">{event.attributes.Name}</h3>
+                    <p className="bg-ariWhite text-ariBlack py-4 max-sm:hyphens-auto text-2xl px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(event.attributes.Price) }} />
+                    <p className="my-4 max-sm:hyphens-auto text-left px-5" dangerouslySetInnerHTML={{ __html: stringWithLineBreaks(event.attributes.Description) }} />
                   </article>
-                )))
-              : (
-                <article className="bg-[#1c1c1a] rounded-md overflow-hidden w-full max-w-full" id='no-events'><h3 className="bg-ariWhite text-ariBlack min-h-[88px] flex items-center justify-center font-bold text-3xl px-5 max-sm:hyphens-auto">No Upcoming Events</h3></article>
-              )
-            )}
+                ))}
+              </div>
+            ) : '')
+          )}
           </div>
         </section>
 
